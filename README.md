e260d867fb3ecae7f6d74f3c159989768232596f
Prashant Agrawal <prashant.agrawal@webklipper.com>
2016-04-25 19:39:13 +0530

    slack bot added

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # RunJS-Slackbot
 A Slack bot that runs javascript (es6) in slack chat
+
+## Installation
+```bash
+git clone git@github.com:howdyai/botkit.git
+cd RunJS-Slackbot
+npm install
+```
+## Setup
+
+1) First make a bot integration inside of your Slack channel. Go here:
+
+https://my.slack.com/services/new/bot
+
+Enter a name for your bot.
+Make it something fun and friendly, but avoid a single task specific name.
+Bots can do lots! Let's not pigeonhole them.
+
+2) When you click "Add Bot Integration", you are taken to a page where you can add additional details about your bot, like an avatar, as well as customize its name & description.
+
+Copy the API token that Slack gives you. You'll need it.
+
+3) Run the example bot app, using the token you just copied:
+​
+```
+token=REPLACE_THIS_WITH_YOUR_TOKEN node bot.js
+```
+4) Invite bot to a channel
+
+Type: `/invite @<my bot>` to invite your bot into channel.
+​
+5) Your bot should be online! Within Slack, send it a quick direct message to say hello. It should say hello back!
+
---
+++ bot.js
@@ -0,0 +1,223 @@
+var Babel = require("babel-core");
+var Entities = require('html-entities').XmlEntities;
+var vm = require('vm')
+entities = new Entities();
+if (!process.env.token) {
+    console.log('Error: Specify token in environment');
+    process.exit(1);
+}
+
+var Botkit = require('./lib/Botkit.js');
+var os = require('os');
+
+var controller = Botkit.slackbot({
+    debug: false,
+});
+
+var bot = controller.spawn({
+    token: process.env.token
+}).startRTM();
+
+controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
+
+    bot.api.reactions.add({
+        timestamp: message.ts,
+        channel: message.channel,
+        name: 'robot_face',
+    }, function(err, res) {
+        if (err) {
+            bot.botkit.log('Failed to add emoji reaction :(', err);
+        }
+    });
+
+
+    controller.storage.users.get(message.user, function(err, user) {
+        if (user && user.name) {
+            bot.reply(message, 'Hello ' + user.name + '!!');
+        } else {
+            bot.reply(message, 'Hello.');
+        }
+    });
+});
+
+controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
+    var name = message.match[1];
+    controller.storage.users.get(message.user, function(err, user) {
+        if (!user) {
+            user = {
+                id: message.user,
+            };
+        }
+        user.name = name;
+        controller.storage.users.save(user, function(err, id) {
+            bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
+        });
+    });
+});
+
+controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function(bot, message) {
+
+    controller.storage.users.get(message.user, function(err, user) {
+        if (user && user.name) {
+            bot.reply(message, 'Your name is ' + user.name);
+        } else {
+            bot.startConversation(message, function(err, convo) {
+                if (!err) {
+                    convo.say('I do not know your name yet!');
+                    convo.ask('What should I call you?', function(response, convo) {
+                        convo.ask('You want me to call you `' + response.text + '`?', [
+                            {
+                                pattern: 'yes',
+                                callback: function(response, convo) {
+                                    // since no further messages are queued after this,
+                                    // the conversation will end naturally with status == 'completed'
+                                    convo.next();
+                                }
+                            },
+                            {
+                                pattern: 'no',
+                                callback: function(response, convo) {
+                                    // stop the conversation. this will cause it to end with status == 'stopped'
+                                    convo.stop();
+                                }
+                            },
+                            {
+                                default: true,
+                                callback: function(response, convo) {
+                                    convo.repeat();
+                                    convo.next();
+                                }
+                            }
+                        ]);
+
+                        convo.next();
+
+                    }, {'key': 'nickname'}); // store the results in a field called nickname
+
+                    convo.on('end', function(convo) {
+                        if (convo.status == 'completed') {
+                            bot.reply(message, 'OK! I will update my dossier...');
+
+                            controller.storage.users.get(message.user, function(err, user) {
+                                if (!user) {
+                                    user = {
+                                        id: message.user,
+                                    };
+                                }
+                                user.name = convo.extractResponse('nickname');
+                                controller.storage.users.save(user, function(err, id) {
+                                    bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
+                                });
+                            });
+
+
+
+                        } else {
+                            // this happens if the conversation ended prematurely for some reason
+                            bot.reply(message, 'OK, nevermind!');
+                        }
+                    });
+                }
+            });
+        }
+    });
+});
+
+
+controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function(bot, message) {
+
+    bot.startConversation(message, function(err, convo) {
+
+        convo.ask('Are you sure you want me to shutdown?', [
+            {
+                pattern: bot.utterances.yes,
+                callback: function(response, convo) {
+                    convo.say('Bye!');
+                    convo.next();
+                    setTimeout(function() {
+                        process.exit();
+                    }, 3000);
+                }
+            },
+        {
+            pattern: bot.utterances.no,
+            default: true,
+            callback: function(response, convo) {
+                convo.say('*Phew!*');
+                convo.next();
+            }
+        }
+        ]);
+    });
+});
+
+
+controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
+    'direct_message,direct_mention,mention', function(bot, message) {
+
+        var hostname = os.hostname();
+        var uptime = formatUptime(process.uptime());
+
+        bot.reply(message,
+            ':robot_face: I am a bot named <@' + bot.identity.name +
+             '>. I have been running for ' + uptime + ' on ' + hostname + '.');
+
+    });
+
+function formatUptime(uptime) {
+    var unit = 'second';
+    if (uptime > 60) {
+        uptime = uptime / 60;
+        unit = 'minute';
+    }
+    if (uptime > 60) {
+        uptime = uptime / 60;
+        unit = 'hour';
+    }
+    if (uptime != 1) {
+        unit = unit + 's';
+    }
+
+    uptime = uptime + ' ' + unit;
+    return uptime;
+}
+
+//My  JS repl
+//TO DO Error handling and reporting
+controller.hears(['js mode'],'direct_message,direct_mention,mention',function(bot, message){
+    bot.startConversation(message,function(err,convo) {
+        var sandbox = {};
+        var context = new vm.createContext(sandbox);
+        convo.say('JS Mode on');
+        var string=';';
+        var sandbox={};
+        convo.ask('', function(response, convo){
+            //convert es6 to es5
+            var ans;
+            try {
+                new Function(entities.decode(response.text));
+                var es5String = Babel.transform(entities.decode(response.text),{ "presets": ["es2015"],highlightCode: false }).code;
+                console.log('es5',es5String);
+                es5String=es5String.replace('"use strict";\n\n','')
+                //run this in sandbox environment
+                // string+=es5String;
+                ans=vm.runInNewContext(es5String, sandbox);
+            } 
+            catch(ex){
+                ans = '['+ex.name+']: '+ex.message;
+            }
+            if(ans===undefined){
+                ans='undefined'
+            } else {
+                ans=JSON.stringify(ans);
+            }
+            
+            convo.say(ans);
+            console.log(ans);
+            convo.repeat();
+            convo.next();
+        });
+
+    });
+
+})
